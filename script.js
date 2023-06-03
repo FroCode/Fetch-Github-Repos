3f37a5c0c60311bce2aa78fae15affb04f3e9253
Haddouche Maamar <frocodeof@gmail.com>
2023-06-03 21:28:10 +0200

    Finsh

--- script.js
+++ script.js
@@ -11,14 +11,22 @@ function getRepos() {
         reposData.innerHTML = "<span>PLease Write GitHub Username.</span>"
     }
     else {
-        fetch("https://api.github.com/users/froCode/repos")
+        fetch(`https://api.github.com/users/${theInput.value}/repos`)
             .then((response) =>  response.json())
             .then((repositories) => {
                 reposData.innerHTML = "";
                 repositories.forEach(repo => {
                     let mainDiv = document.createElement("div")
                     let repoName = document.createTextNode(repo.name)
+                    let repoUrl = document.createElement("a")
+                    // create repo text
+                    let theUrlText = document.createTextNode("Visit")
+                    repoUrl.appendChild(theUrlText)
+                    repoUrl.href = `https:/github.com/FroCode/${repo.name}`
+                    repoUrl.setAttribute('target', '_blank')
+                    mainDiv.appendChild(repoUrl)
                     mainDiv.appendChild(repoName)
+                    reposData.appendChild(mainDiv)
                 })
             })
 
